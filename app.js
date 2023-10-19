60563ce61c7be32fba978760b233c1b323443983
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2023-10-18 20:36:16 -0700

    finished app.js

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express = require("express");
 const { PORT = 3001 } = process.env;
 const app = express();
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
-
 const routes = require("./routes");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((e) => console.log("db error", e));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "6530a12d11503c43beaeedd7",
+  };
+  next();
+});
+
 app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
-  console.log("LOL");
 });
