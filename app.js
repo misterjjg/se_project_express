9d867547d727075ad6e8e913e716ff25566b6880
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2023-10-19 10:12:45 -0700

    fixed helmet

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
-const routes = require("./routes");
-
 const helmet = require("helmet");
+
+const routes = require("./routes");
 
 const { PORT = 3001 } = process.env;
 const app = express();
