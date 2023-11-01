dd06c4313302c4445a4e7a96e94eb9b4c4c32abc
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2023-11-01 16:48:48 -0700

    added middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/constants");
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.status(401).send({ message: "Authorization required" });
+  }
+
+  const token = authorization.replace("Bearer ", "");
+
+  let payload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    console.error(err);
+    return res.status(403).send({ message: "Forbidden Access" });
+  }
+
+  req.user = payload;
+  next();
+};
