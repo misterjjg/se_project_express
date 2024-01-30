03ca0c403a5234db1f909494c354663f74e80e0a
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2024-01-29 21:50:28 -0800

    update user route

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,14 @@ const auth = require("../middlewares/auth");
 
 const { updateCurrentUser, getCurrentUser } = require("../controllers/user");
 
+const { validateUpdateUser } = require("../middlewares/validation");
+
 // Create
 
 // Read
 router.get("/me", auth, getCurrentUser);
 
 // Update
-router.patch("/me", auth, updateCurrentUser);
+router.patch("/me", auth, validateUpdateUser, updateCurrentUser);
 
 module.exports = router;
