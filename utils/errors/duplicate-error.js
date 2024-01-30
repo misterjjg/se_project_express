dd4db86d1ea245bd75ecc32c1bd4976134f652cc
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2024-01-29 21:48:29 -0800

    created error folder

---
+++ utils/errors/duplicate-error.js
@@ -0,0 +1,9 @@
+class DuplicateError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 409;
+    this.name = "DuplicateError";
+  }
+}
+
+module.exports = DuplicateError;
