a0f33dabfda2e6fd2d319a686bc22bb469b71bff
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2024-01-29 22:25:50 -0800

    updated auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,15 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/constants");
-const { ERROR_401 } = require("../utils/errors");
+// const { ERROR_401 } = require("../utils/errors");
+const UnauthorizedError = require("../utils/errors/unauthorized-error");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res.status(ERROR_401).send({ message: "Authorization required" });
+    return next(
+      new UnauthorizedError("Invalid format for authorization header"),
+    );
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -16,7 +19,9 @@ module.exports = (req, res, next) => {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     console.error(err);
-    return res.status(ERROR_401).send({ message: "Forbidden Access" });
+    return next(
+      new UnauthorizedError("An error occurred while parsing the payload"),
+    );
   }
 
   req.user = payload;
